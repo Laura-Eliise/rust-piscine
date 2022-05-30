04d52330650cb6853c26ac78dda515ca0e3c94c2
Laura-Eliise <laura-eliise@marrandi.ee>
2022-05-30 11:40:08 +0300

    lights adjusted

---
+++ data/changes/target/debug/deps/changes-4538379dd77484e0.d
@@ -0,0 +1,5 @@
+/home/laura-eliise/Desktop/01/piscine-rust/data/changes/target/debug/deps/changes-4538379dd77484e0: src/main.rs
+
+/home/laura-eliise/Desktop/01/piscine-rust/data/changes/target/debug/deps/changes-4538379dd77484e0.d: src/main.rs
+
+src/main.rs:
