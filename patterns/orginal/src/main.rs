10a2e7e35eacb3dbea556527ae908e4b1282fac2
Laura-Eliise <laura-eliise@marrandi.ee>
2022-06-02 14:02:53 +0300

    done

--- patterns/orginal/src/main.rs
+++ patterns/orginal/src/main.rs
@@ -1,12 +1,14 @@
 fn main() {
     println!("{}", num_to_ordinal(1));
-    println!("{}", num_to_ordinal(22));
+    println!("{}", num_to_ordinal(21));
     println!("{}", num_to_ordinal(43));
     println!("{}", num_to_ordinal(47));
 }
 
 pub fn num_to_ordinal(x: u32) -> String {
     let s = x.to_string();
+    let lastTwo: String = s.chars().rev().take(2).collect();
+    if lastTwo == "21" || lastTwo == "11" {return s + "th"}
     return s.clone() + match s.chars().last().unwrap() {
         '1' => "st",
         '2' => "nd",
